fbfee4a8d09d659cd8a3918aaeb46a558243d28f
sampada491 <71688485+sampada491@users.noreply.github.com>
2021-01-07 16:54:13 +0530

    Update SpeakGoodBye.js

--- mod4_sol/SpeakGoodBye.js
+++ mod4_sol/SpeakGoodBye.js
@@ -10,7 +10,7 @@
 // See Lecture 52, part 1
 (function(window){
   
-  
+  var byeSpeaker={};
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
 var speakWord = "Good Bye";
@@ -18,7 +18,7 @@ var speakWord = "Good Bye";
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-var byeSpeaker =function (name) {
+byeSpeaker.speak =function (name) {
   console.log(speakWord + " " + name);
 }
 
