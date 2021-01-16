b2b9d63b8396bc91191824a13eee8603f4fc6833
sampada491 <71688485+sampada491@users.noreply.github.com>
2021-01-16 12:54:59 +0530

    Update script.js

--- mod4_sol/script.js
+++ mod4_sol/script.js
@@ -46,27 +46,12 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // See Lecture 50, part 1
 for (var name in names) {
  var firstLetter = names[name].charAt(0).toLowerCase();
- if (firstLetter==='j') {
-     byeSpeaker.Speak(names[name]);
+ if (firstLetter=='j') {
+     byeSpeaker.speak(names[name]);
   } else {
-    helloSpeaker.Speak(names[name]);
+    helloSpeaker.speak(names[name]);
   }
  
 }
-  // STEP 11:
-  // Retrieve the first letter of the current name in the loop.
-  // Use the string object's 'charAt' function. Since we are looking for
-  // names that start with either upper case or lower case 'J'/'j', call
-  // string object's 'toLowerCase' method on the result so we can compare
-  // to lower case character 'j' afterwards.
-  // Look up these methods on Mozilla Developer Network web site if needed.
-  
-  // STEP 12:
-  // Compare the 'firstLetter' retrieved in STEP 11 to lower case
-  // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
-  // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
-  // name in the loop.
-  
-
-
+ 
       })();
